111fd7b0140453feff9af20065a4b67991876299
ivgtr <gear.1112t.o@gmail.com>
2021-02-09 13:34:37 +0900

    :sparkles: add: metaタグをとりあえず追加

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -10,8 +10,12 @@ const App = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
+        <title>アイカツ！格言ボタン</title>
+        <meta name="description" content="アイカツ！格言が再生されるボタン置き場。"></meta>
         <meta name="viewport" content="width=device-width,height=device-height" key="viewport" />
         <meta name="theme-color" content="#087da1" key="themeColor" />
+        <meta property="og:image" content="/aikatsu.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
       </Head>
       <RecoilRoot>
         <Control />
