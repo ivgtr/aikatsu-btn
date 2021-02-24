0cd58b85bc50596c37d48f835113266413d921ef
ivgtr <gear.1112t.o@gmail.com>
2021-02-24 15:14:30 +0900

    :lipstick: change: デザイン調整

--- src/components/Ad.tsx
+++ src/components/Ad.tsx
@@ -1,35 +1,20 @@
 import React from 'react'
-import { Timeline } from 'react-twitter-widgets'
 
 const Ad: React.FC = () => {
   return (
     <>
-      <h2>
+      <h2 className="mt-4">
         毎週日曜あさ7時大好評放送中！{' '}
         <a
           href="http://www.aikatsu.net/"
           target="_brank"
-          className="inline-block cursor-pointer text-blue-400 border-b border-blue-400"
+          className="inline-block cursor-pointer text-blue-400 underline"
         >
           アイカツプラネット！
         </a>
       </h2>
-
-      <div className="flex flex-col sm:flex-row items-center">
-        <div className="max-w-xs m-2">
-          <Timeline
-            dataSource={{
-              sourceType: 'profile',
-              screenName: 'aikatsu_anime'
-            }}
-            options={{
-              height: '400'
-            }}
-          />
-        </div>
-        <div className="max-w-md m-2">
-          <img src="/logo.jpg" alt="logo" className="w-full" />
-        </div>
+      <div className="max-w-md m-2">
+        <img src="/logo.jpg" alt="logo" className="w-full" />
       </div>
     </>
   )
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,6 @@
 import React, { FC } from 'react'
+import { Timeline } from 'react-twitter-widgets'
+
 import Button from '../components/Button'
 import Ad from '../components/Ad'
 import kakugen from '../assets/kakugen.json'
@@ -6,7 +8,7 @@ import kakugen from '../assets/kakugen.json'
 const Home: FC = () => {
   return (
     <div className="container mx-auto px-2">
-      <h1 className="text-center my-12 text-2xl font-bold">アイカツ！格言ボタン</h1>
+      <h1 className="text-center mt-12 text-2xl font-bold">アイカツ！格言ボタン</h1>
       <Ad />
       <article>
         <section className="my-8">
@@ -26,7 +28,7 @@ const Home: FC = () => {
               return item.season === '2'
             })
             .map((item, index) => {
-              return <Button key={`s1${index}`} kakugen={item} />
+              return <Button key={`s2${index}`} kakugen={item} />
             })}
         </section>
         <section className="my-8">
@@ -36,10 +38,22 @@ const Home: FC = () => {
               return item.season === '3'
             })
             .map((item, index) => {
-              return <Button key={`s1${index}`} kakugen={item} />
+              return <Button key={`s3${index}`} kakugen={item} />
             })}
         </section>
       </article>
+
+      <div className="max-w-xs m-2">
+        <Timeline
+          dataSource={{
+            sourceType: 'profile',
+            screenName: 'aikatsu_anime'
+          }}
+          options={{
+            height: '400'
+          }}
+        />
+      </div>
     </div>
   )
 }
